3049627e2dd8dbab3508e550e09137c76b16aaf6
avneeshnaha <nahaavneesh@gmail.com>
2023-10-17 14:28:16 -0700

    Add blue background and color to website

--- index.php
+++ index.php
@@ -1,5 +1,56 @@
 <!DOCTYPE html>
 <html>
+<?php
+session_start(); // Start the session
+
+// Define a simple array of user data (in a real application, this would come from a database)
+$users = [
+    ['username' => 'user1', 'password' => 'password1'],
+    ['username' => 'user2', 'password' => 'password2'],
+];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Loop through the user array and check for a matching user
+    foreach ($users as $user) {
+        if ($user['username'] === $username && $user['password'] === $password) {
+            $_SESSION['username'] = $username;
+            header("Location: dashboard.php"); // Redirect to the dashboard after successful login
+            exit();
+        }
+    }
+
+    $error = "Invalid username or password";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Log In</button>
+    </form>
+
+    <?php
+    if (isset($error)) {
+        echo "<p style='color: red;'>$error</p>";
+    }
+    ?>
+</body>
+</html>
 <head>
     <meta charset="UTF-8">
     <title>Simple Website</title>
